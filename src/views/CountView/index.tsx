ef2b8bb97c63b1734931e1eeddc2afd97918e907
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2025-08-03 22:24:13 +0900

    add img

--- src/views/CountView/index.tsx
+++ src/views/CountView/index.tsx
@@ -1,5 +1,7 @@
 import React from "react"
+import Image from "next/image"
 import { useCountView } from "./hooks"
+import { config } from "~/config"
 
 export const CountView = React.memo(() => {
   const { count, addCount } = useCountView()
@@ -10,6 +12,12 @@ export const CountView = React.memo(() => {
       <button data-testid="button" className="bg-red-500 px-2 py-4 hover:bg-red-300" onClick={addCount}>
         カウントアップ==
       </button>
+      <Image
+        src={`${config.imageUrl}tests.svg`}
+        alt="placeholder"
+        width={100}
+        height={100}
+      />
     </div>
   )
 })
